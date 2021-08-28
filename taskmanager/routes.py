d5460eabec36eadcafca168eb61267e965f8bce4
John Traas <j.j.h.traas@gmail.com>
2021-08-28 22:36:17 +0000

    add inital basic routes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,4 +5,19 @@ from taskmanager.models import Category, Task, Day
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template('categories.html')
+
+
+@app.route("/add_category", methods=("GET", "POST"))
+def add_category():
+    return render_template("add_category.html")
+
+
+@app.route("/add_task")
+def add_task():
+    return render_template("add_task.html")
