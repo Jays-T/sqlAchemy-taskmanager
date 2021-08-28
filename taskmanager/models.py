32f8bc3b49f344efa24f85f85791d401e74355bd
John Traas <j.j.h.traas@gmail.com>
2021-08-28 22:36:03 +0000

    Write database models

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,5 +1,44 @@
 from taskmanager import db
 
+class UserAccount(db.Model):
+    """
+    User capable of adding tasks
+
+    :param str email: email address of user
+    :param str username: username of user
+    :param str password: encrypted password for the user
+    
+    """
+    __tablename__ = "user_account"
+
+    email = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(12), nullable=False)
+    password = db.Column(db.String)
+    authenticated = db.Column(db.Boolean, default=False)
+    tasks = db.relationship("Task", backref="user", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        return self.username
+
+    def user_tasks(self):
+        return self.tasks
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
 
 class Category(db.Model):
     # schema for Category model
@@ -13,6 +52,8 @@ class Category(db.Model):
 
 class Task(db.Model):
     # schema for Task model
+    user = db.Column(db.Integer, db.ForeignKey('user_account.email', ondelete="CASCADE"), nullable=False)
+    created_by = db.relationship("UserAccount", backref="user_account.username", lazy=True)
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -22,8 +63,8 @@ class Task(db.Model):
     day_id = db.Column(db.Integer, db.ForeignKey("day.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return "#{0} - Task: {1} | Urgent: {2} | Day: {3}".format(
-            self.id, self.task_name, self.is_urgent, self.day_id
+        return "#{0} - Task: {1} | Urgent: {2} | Day: {3} | Created By: {4}".format(
+            self.id, self.task_name, self.is_urgent, self.day_id, self.created_by
         )
 
 
